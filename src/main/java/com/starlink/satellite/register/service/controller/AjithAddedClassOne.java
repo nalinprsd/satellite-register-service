b63e03c9a16abb646cce0673c63e6cba17aba560
Ajith <ajithkumasl7@gmail.com>
2024-07-16 23:03:05 +0200

    Ajith commit 1

---
+++ src/main/java/com/starlink/satellite/register/service/controller/AjithAddedClassOne.java
@@ -0,0 +1,20 @@
+package com.starlink.satellite.register.service.controller;
+
+public class AjithAddedClassOne {
+    double value;
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "AjithAddedClassOne{" +
+                "value=" + value +
+                '}';
+    }
+}
