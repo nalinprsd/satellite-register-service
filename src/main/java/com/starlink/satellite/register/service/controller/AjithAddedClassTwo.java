11f375d33de9eeb9f651dfc14a95cb8875e7689d
Ajith <ajithkumasl7@gmail.com>
2024-07-17 08:53:07 +0200

    Ajith commit 1

---
+++ src/main/java/com/starlink/satellite/register/service/controller/AjithAddedClassTwo.java
@@ -0,0 +1,13 @@
+package com.starlink.satellite.register.service.controller;
+
+public class AjithAddedClassTwo {
+    String homeAddress;
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+}
