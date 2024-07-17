a25da46e25ae8c23f28e590364167024fe761ffb
Ajith <ajithkumasl7@gmail.com>
2024-07-17 18:03:30 +0200

    Prasad commit 1

---
+++ src/main/java/com/starlink/satellite/register/service/pojo/PrasadAddedClassFive.java
@@ -0,0 +1,9 @@
+package com.starlink.satellite.register.service.pojo;
+
+public class PrasadAddedClassFive {
+    String test;
+
+    public PrasadAddedClassFive(String test) {
+        this.test = test;
+    }
+}
