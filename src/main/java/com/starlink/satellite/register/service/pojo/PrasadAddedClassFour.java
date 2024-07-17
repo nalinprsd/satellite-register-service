7839fd884d8bfd98710799d60a431c72101e7948
Ajith <ajithkumasl7@gmail.com>
2024-07-17 18:17:56 +0200

    Prasad commit 2

--- src/main/java/com/starlink/satellite/register/service/pojo/PrasadAddedClassFour.java
+++ src/main/java/com/starlink/satellite/register/service/pojo/PrasadAddedClassFour.java
@@ -2,5 +2,6 @@ package com.starlink.satellite.register.service.pojo;
 
 public class PrasadAddedClassFour {
     private String myLocation;
+    private String newFIeld;
 
 }
---
+++ src/main/java/com/starlink/satellite/register/service/pojo/PrasadAddedClassSix.java
@@ -0,0 +1,14 @@
+package com.starlink.satellite.register.service.pojo;
+
+public class PrasadAddedClassSix {
+
+    String placeOfBirth;
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+}
